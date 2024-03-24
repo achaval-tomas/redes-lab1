f0999dc51d0798ac7480ee0ff3048bb770089a20
Tomás Peyronel <tpeyronel@gmail.com>
2024-03-24 12:56:44 -0300

    arreglar warnings in proximo_feriado

--- proximo_feriado.py
+++ proximo_feriado.py
@@ -1,14 +1,20 @@
 import requests
 from datetime import date
 
+
 def get_url(year):
     return f"https://nolaborables.com.ar/api/v2/feriados/{year}"
 
-months = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
-days = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo']
+
+months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio',
+          'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+days = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado',
+        'Domingo']
+
 
 def day_of_week(day, month, year):
     return days[date(year, month, day).weekday()]
+
 
 class NextHoliday:
     def __init__(self):
@@ -24,8 +30,11 @@ class NextHoliday:
         }
 
         holiday = next(
-            (h for h in holidays if (h['mes'] == today['month'] and h['dia'] > today['day']
-                                     or h['mes'] > today['month']) and (kind is None or h['tipo'] == kind)),
+            (h for h in holidays
+             if (h['mes'] == today['month']
+                 and h['dia'] > today['day']
+                 or h['mes'] > today['month'])
+                and (kind is None or h['tipo'] == kind)),
             holidays[0]
         )
 
@@ -44,11 +53,14 @@ class NextHoliday:
             print("Próximo feriado")
             print(self.holiday['motivo'])
             print("Fecha:")
-            print(day_of_week(self.holiday['dia'], self.holiday['mes'], self.year))
+            print(day_of_week(self.holiday['dia'],
+                              self.holiday['mes'],
+                              self.year))
             print(self.holiday['dia'])
             print(months[self.holiday['mes']-1])
             print("Tipo:")
             print(self.holiday['tipo'])
+
 
 next_holiday = NextHoliday()
 next_holiday.fetch_holidays()
